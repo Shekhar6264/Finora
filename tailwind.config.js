ac7642ddaa3f01b37128a1c39e0b6d966779caf5
Shekhar6264 <shekharjamalpuri18@gmail.com>
2026-04-05 20:01:37 +0530

    Fixed CSS nesting issue finally

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          950: "#030303",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
